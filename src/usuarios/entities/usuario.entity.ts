ce2c28377888acda4dee29d9735d239847cb73c9
Drakthor475 <canterodiego17@gmail.com>
2025-10-17 13:52:11 -0600

    recursos

--- src/usuarios/entities/usuario.entity.ts
+++ src/usuarios/entities/usuario.entity.ts
@@ -1,14 +1,9 @@
-import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
-
-
+import { Column, Entity, ManyToOne, OneToMany, PrimaryGeneratedColumn } from "typeorm";
 
 @Entity()
 export class Usuario {
     @PrimaryGeneratedColumn()
     id_usuario: number
-
-    @Column({nullable:true})
-    id_tipo_usuario:number
 
     @Column({length:100, nullable:true})
     nombre:string
@@ -16,16 +11,21 @@ export class Usuario {
     @Column({length:100,nullable:true})
     contraseña:string
     
-   
-    
+    @ManyToOne(()=>Tipo_Usuario,tipoUsuario=>tipoUsuario.usuarios )
+    tipoUsuario:Tipo_Usuario;
 
 }
+
 @Entity()
 export class Tipo_Usuario{
     
     @PrimaryGeneratedColumn()
     id_tipo_usuario:number
+
     @Column({nullable:true})
     tipo_usuario:string
+
+    @OneToMany(()=>Usuario,usuarios=>usuarios.tipoUsuario)
+    usuarios:Usuario[];
 
 }
