71794f84eaa9e29b4c7680f786c35f4c727f186f
Drakthor475 <canterodiego17@gmail.com>
2025-10-17 12:51:12 -0600

    docker

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,32 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import {ConfigModule} from '@nestjs/config'
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+
+
+
 
 @Module({
-  imports: [],
-  controllers: [AppController],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal:true,
+    }),
+    TypeOrmModule.forRoot({
+      type:'mariadb',
+      host:process.env.DB_HOST,
+      port:Number(process.env.DB_PORT),
+      username: process.env.DB_USER,
+      password: process.env.DB_PASS,
+      database: process.env.DB_NAME,
+      autoLoadEntities:true,
+      synchronize:true,
+
+
+    })
+  ],
+  controllers: [],
   providers: [AppService],
 })
 export class AppModule {}
