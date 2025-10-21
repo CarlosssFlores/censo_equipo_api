2cafeaff0bc2a8943c50ae1365f339acb576af1f
CarlosssFlores <307100636@pcpuma.acatlan.unam.mx>
2025-10-21 15:07:59 -0600

    entities

---
+++ src/equipo/entities/catalogo.entities.ts
@@ -0,0 +1,85 @@
+import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
+import { Equipo } from './equipo.entity';
+
+@Entity()
+export class Uso {
+  @PrimaryGeneratedColumn()
+  id_tipo_uso: number;
+
+  @Column()
+  tipo_uso: string;
+  @OneToMany(() => Equipo, (equipo) => equipo.tipoUso)
+  equipo: Equipo;
+}
+
+@Entity()
+export class Marca {
+  @PrimaryGeneratedColumn()
+  id_marca: number;
+
+  @Column()
+  tipo_marca: string;
+
+  @OneToMany(() => Equipo, (equipo) => equipo.marca)
+  equipo: Equipo;
+}
+
+@Entity()
+export class Estado {
+  @PrimaryGeneratedColumn()
+  id_estado: number;
+
+  @Column()
+  estado: string;
+
+  @OneToMany(() => Equipo, (equipo) => equipo.estado)
+  equipo: Equipo;
+}
+
+@Entity()
+export class Adscripcion {
+  @PrimaryGeneratedColumn()
+  id_adscripcion: number;
+
+  @Column()
+  adscripcion: string;
+
+  @OneToMany(() => Equipo, (equipo) => equipo.adscripcion)
+  equipo: Equipo;
+}
+
+@Entity()
+export class TipoEquipo {
+  @PrimaryGeneratedColumn()
+  id_tipo_de_equipo: number;
+
+  @Column()
+  tipo_equipo: string;
+
+  @OneToMany(() => Equipo, (equipo) => equipo.tipoEquipo)
+  equipo: Equipo;
+}
+
+@Entity()
+export class SistemaOperativo {
+  @PrimaryGeneratedColumn()
+  id_sistema_operativo: number;
+
+  @Column()
+  sistema_operativo: string;
+
+  @OneToMany(() => Equipo, (equipo) => equipo.sistemaOperativo)
+  equipo: Equipo;
+}
+
+@Entity()
+export class Procesador {
+  @PrimaryGeneratedColumn()
+  id_procesador: number;
+
+  @Column()
+  procesador: string;
+
+  @OneToMany(() => Equipo, (equipo) => equipo.procesador)
+  equipo: Equipo;
+}
--- src/usuarios/entities/usuario.entity.ts
+++ src/usuarios/entities/usuario.entity.ts
@@ -1,31 +1,34 @@
-import { Column, Entity, ManyToOne, OneToMany, PrimaryGeneratedColumn } from "typeorm";
+import {
+  Column,
+  Entity,
+  ManyToOne,
+  OneToMany,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
 
 @Entity()
 export class Usuario {
-    @PrimaryGeneratedColumn()
-    id_usuario: number
+  @PrimaryGeneratedColumn()
+  id_usuario: number;
 
-    @Column({length:100, nullable:true})
-    nombre:string
+  @Column({ length: 100, nullable: true })
+  nombre: string;
 
-    @Column({length:100,nullable:true})
-    contraseña:string
-    
-    @ManyToOne(()=>Tipo_Usuario,tipoUsuario=>tipoUsuario.usuarios )
-    tipoUsuario:Tipo_Usuario;
+  @Column({ length: 100, nullable: true })
+  contraseña: string;
 
+  @ManyToOne(() => Tipo_Usuario, (tipoUsuario) => tipoUsuario.usuarios)
+  tipoUsuario: Tipo_Usuario;
 }
 
 @Entity()
-export class Tipo_Usuario{
-    
-    @PrimaryGeneratedColumn()
-    id_tipo_usuario:number
+export class Tipo_Usuario {
+  @PrimaryGeneratedColumn()
+  id_tipo_usuario: number;
 
-    @Column({nullable:true})
-    tipo_usuario:string
+  @Column({ nullable: true })
+  tipo_usuario: string;
 
-    @OneToMany(()=>Usuario,usuarios=>usuarios.tipoUsuario)
-    usuarios:Usuario[];
-
+  @OneToMany(() => Usuario, (usuarios) => usuarios.tipoUsuario)
+  usuarios: Usuario[];
 }
